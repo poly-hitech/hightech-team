6870acadc3db5217de42e2494c3e24658499e927
wlwl45wl <wlwl45wl@gmail.com>
2025-10-03 20:20:35 +0900

    deploy

--- src/main/java/com/example/test/Model/HostAdress.java
+++ src/main/java/com/example/test/Model/HostAdress.java
@@ -2,12 +2,11 @@ package com.example.test.Model;
 
 public class HostAdress {
 	private String host;
-	private String address = "https://wookportfolio.duckdns.org:83/";
 
 	public String getHost() {
 		return host;
 	}
 	public void setHost() {
-		this.host = this.address;
+		this.host = "https://wookportfolio.duckdns.org:83/";
 	}
 }
