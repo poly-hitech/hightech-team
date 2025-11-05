cc2c2db6c7f52b982691d74ef21b946c40712b1e
Elephant24456 <rlaxortn0984@naver.com>
2025-11-05 17:54:21 +0900

    ForumPostFile 모델 생성

---
+++ src/main/java/com/example/test/Model/ForumPostFile.java
@@ -0,0 +1,19 @@
+package com.example.test.Model;
+
+public class ForumPostFile {
+	private long postId;
+	private String fileName;
+	
+	public long getPostId() {
+		return postId;
+	}
+	public void setPostId(long postId) {
+		this.postId = postId;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+}
